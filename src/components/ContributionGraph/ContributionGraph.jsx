1692e3b0137cb34cf6f8d11edf238be79d540d88
cauliflow3r <dzhirtanov.a@gmail.com>
2023-11-28 18:55:08 +0600

    at least its working correct now

--- src/components/ContributionGraph/ContributionGraph.jsx
+++ src/components/ContributionGraph/ContributionGraph.jsx
@@ -5,27 +5,36 @@ import styles from "./ContributionGraph.module.css";
 const ContributionGraph = () => {
   const [contributions, setContributions] = useState({});
 
+  // получаем данные
+
   async function getData() {
     const response = await fetch("https://dpg.gg/test/calendar.json");
     const data = await response.json();
 
+    // решил что будет лучше мэпать уже готовые данные
     const newData = {};
 
-    for (let i = 1; i <= 357; i++) {
-      const currentDate = new Date();
-      currentDate.setDate(currentDate.getDate() - i + 1);
+    const currentDate = new Date();
+    const daysUntilSunday = (7 - currentDate.getDay()) % 7;
+    currentDate.setDate(currentDate.getDate() + daysUntilSunday);
 
+    for (let i = 0; i < 357; i++) {
       const formattedDate = currentDate.toISOString().split("T")[0];
 
       newData[formattedDate] = [
         data[formattedDate] || 0,
         getColor(data[formattedDate] || 0),
       ];
+
+      currentDate.setDate(currentDate.getDate() - 1);
+      // console.log("formattedDate", formattedDate);
     }
+
     setContributions(newData);
-    console.log(newData);
-    console.log(Object.keys(newData).length);
+    // console.log(newData);
+    // console.log(Object.keys(newData).length);
   }
+  //! пихаем цвет сразу в наш объект )
   const getColor = (value) => {
     if (value === 0) {
       return "#EDEDED";
@@ -39,36 +48,87 @@ const ContributionGraph = () => {
       return "#254E77";
     }
   };
-  const onMouseOver = (date) => {
-    console.log(date);
-  };
+
   useEffect(() => {
     getData();
   }, []);
 
+  // база
+
+  // Months (убил больше времени чем хотел)
+  const renderMonthLabels = () => {
+    const monthLabels = [];
+    const currentDate = new Date();
+    for (let i = 0; i < 12; i++) {
+      const month = currentDate.toLocaleString("ru-ru", { month: "short" });
+      monthLabels.push(month);
+      currentDate.setMonth(currentDate.getMonth() - 1);
+    }
+    return monthLabels.reverse();
+  };
+
+  const renderDayLabels = () => {
+    const dayLabels = ["Пн", "", "Ср", "", "Пт", "", ""];
+    // костыль хы )
+    return dayLabels;
+  };
+  const formatDate = (dateString) => {
+    const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
+
+    const date = new Date(dateString);
+    return date.toLocaleDateString("ru-RU", options);
+  };
+  // todo: то что ниже надо рефакторить и пихать в компоненты
   return (
     <div className={styles.container}>
-      <div className={styles.graph}>
-        {Object.keys(contributions)
-          .reverse()
-          .map((date) => (
-            <div
-              className={styles.square}
-              key={date}
-              date={date}
-              style={{
-                backgroundColor: contributions[date][1],
-              }}
-            >
-              <span className={styles.tooltipText}>
-                <h5>{`${contributions[date][0]} Contribution`}</h5>
-                <p>{[date]}</p>
-              </span>
+      <div className={styles.monthLabels}>
+        {renderMonthLabels().map((month, index) => (
+          <div key={index} className={styles.monthLabel}>
+            {month.charAt(0).toUpperCase() + month.slice(1)}
+          </div>
+        ))}
+      </div>
+      <div className={styles.bottomContainer}>
+        <div className={styles.leftLabels}>
+          {renderDayLabels().map((day, index) => (
+            <div key={index} className={styles.dayLabel}>
+              {day}
             </div>
           ))}
+        </div>
+        <div className={styles.graph}>
+          {Object.keys(contributions)
+            .reverse()
+            .map((date) => (
+              <div
+                className={styles.square}
+                key={date}
+                date={date}
+                style={{
+                  backgroundColor: contributions[date][1],
+                }}
+              >
+                <div className={styles.tooltipText}>
+                  <h5>{`${contributions[date][0]} Contribution`}</h5>
+                  <p>{formatDate(date)}</p>
+                  <div className={styles.pointerContainer}>
+                    <div className={styles.pointer}></div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default ContributionGraph;
+
+// todo:  code refactoring ideas for the future, move most of the helper functions in a sepperate file for better readability.
+// make smaller components for better usability
